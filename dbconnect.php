66b143f2d96255b93d0e7ab2215f878c1f594a66
aniketsingh <aniket.singh@quovantis.com>
2018-10-30 14:46:32 +0530

    first commit

---
+++ dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+$time_zone = 'Asia/Kolkata';
+
+$con = mysqli_connect("localhost", "saikat", "ppioneer", "todo_db");
+
+if(!$con) {
+    echo "Error connecting to MySQL <br>";
+    exit;
+}
+?>
